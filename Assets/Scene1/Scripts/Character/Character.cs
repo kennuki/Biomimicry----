ee766d7381cb4141909cfe0282e1b3ebb303f9b6
kennuki <henry0002277@gmail.com>
2023-03-27 23:13:55 +0800

    3/27

--- Assets/Scene1/Scripts/Character/Character.cs
+++ Assets/Scene1/Scripts/Character/Character.cs
@@ -5,29 +5,30 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     CharacterController controller;
+    public GameObject LookPoint;
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        CdHeight = controller.height;
+        LookpointOriginY = LookPoint.transform.position.y;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        SquatFunction();
         MoveFunction();
-        move.y = move.y - v1 * 15 * Time.deltaTime;
+        GravityFunction();
         controller.Move(move * Time.deltaTime);
     }
+
+    #region MoveFunction
     public static float speed = 5f;
     public static float imaangle;
     Vector3 move;
     private Vector3 dir;
-    float h;
-    float j;
-    float a = 0;
-    float g = 1f, v1 = 4, c = 0;
+    float h, j;  //Horizontal input, Vertical input
     private void MoveFunction()
     {
-        a += Time.deltaTime;
         h = Input.GetAxis("Horizontal");
         j = Input.GetAxis("Vertical");
         imaangle = transform.eulerAngles.y * Mathf.Deg2Rad;
@@ -40,11 +41,75 @@ public class Character : MonoBehaviour
         {
             dir /= Mathf.Sqrt(2);
         }
-
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
-        {
-            a = 1;
-        }
         move = dir * speed;
     }
+    #endregion
+    #region SquatFunction
+    int SquatState; //0 = stand, 1 = Squat, 2 = Lie down and look
+    float count = 0;
+    bool stay = false;
+    float CdHeight;
+    float LookpointOriginY;
+    private void SquatFunction()
+    {
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            count += Time.deltaTime;
+            if (count > 0.5f)
+            {
+                SquatState = 2;
+            }
+            else if (count > 0.1f && SquatState == 1)
+            {
+                SquatState = 2;
+            }
+            else if (stay == false)
+            {
+                SquatState = Mathf.Abs(SquatState - 1);
+                stay = true;
+            }
+        }
+        else
+        {
+            if (SquatState == 2)
+            {
+                SquatState = 1;
+            }
+            count = 0;
+            stay = false;
+        }
+        if (SquatState == 1)
+        {
+            controller.height = Mathf.Clamp(controller.height - Time.deltaTime, CdHeight / 2, CdHeight);
+            controller.center = new Vector3(0, Mathf.Clamp(controller.center.y - Time.deltaTime * 0.5f, 0 - CdHeight * 0.25f, 0), 0);
+            LookPoint.transform.position = new Vector3(LookPoint.transform.position.x, Mathf.Clamp(LookPoint.transform.position.y-Time.deltaTime,LookpointOriginY- CdHeight * 0.25f, LookpointOriginY), LookPoint.transform.position.z);
+        }
+        else if(SquatState == 0)
+        {
+            controller.height = Mathf.Clamp(controller.height + Time.deltaTime, CdHeight / 2, CdHeight);
+            controller.center = new Vector3(0, Mathf.Clamp(controller.center.y + Time.deltaTime * 0.5f, 0 - CdHeight * 0.25f, 0), 0);
+            LookPoint.transform.position = new Vector3(LookPoint.transform.position.x, Mathf.Clamp(LookPoint.transform.position.y + Time.deltaTime, LookpointOriginY - CdHeight * 0.25f, LookpointOriginY), LookPoint.transform.position.z);
+        }
+    }
+
+
+    #endregion
+    #region GravityFunction
+    float g = 1f, v1 = 10, c = 0;
+    private void GravityFunction()
+    {
+        c += Time.deltaTime;
+        if (!controller.isGrounded)
+        {
+            v1 = v1 + g;
+            c = 0;
+        }
+        else if (controller.isGrounded && c > 0.05f)
+        {
+            v1 = 10;
+        }
+        move.y = move.y - v1 * 5 * Time.deltaTime;
+    }
+    #endregion
 }
