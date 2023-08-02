162b50895d35718b4f066172cf899043a7c37d0e
kennuki <henry0002277@gmail.com>
2023-08-02 17:25:21 +0800

    123

--- Assets/Scenes/Scene1/Scripts/Camera/ScanCamera.cs
+++ Assets/Scenes/Scene1/Scripts/Camera/ScanCamera.cs
@@ -13,7 +13,6 @@ public class ScanCamera : MonoBehaviour
         Cm = GetComponent<Camera>();
         CharacterCamera = GameObject.Find("CM vcam1").transform;
         Field_Of_View = Cm.fieldOfView;
-        GlassRotate = TargetGlass.rotation;
         if(FixAxis == "X")
         {
             OriginAxisPosition = transform.position;
@@ -26,13 +25,18 @@ public class ScanCamera : MonoBehaviour
             MaxP = MirrorScale / 2;
             MinP = -MirrorScale / 2;
         }
+        transform.Rotate(AngleOffset, 0, 0);
+        GlassRotate = TargetGlass.rotation;
+        OriginTargetRotation = TargetGlass.eulerAngles;
+        OriginRotation = transform.eulerAngles;
     }
     public float AngleOffset = -45;
     private void Update()
     {
         CameraFix();
         transform.LookAt(new Vector3(CharacterCamera.transform.position.x, CharacterCamera.transform.position.y+ CameraY_Offset, CharacterCamera.transform.position.z));
-        transform.Rotate(AngleOffset, 0, 0);
+        CameraFix2();
+
     }
 
     public string FixAxis = "X";
@@ -43,10 +47,11 @@ public class ScanCamera : MonoBehaviour
     private float Field_Of_View;
     private void CameraFix()
     {
+        GlassRotate = TargetGlass.rotation;
         float Distance = Vector3.Distance(transform.position, CharacterCamera.transform.position);
         Vector3 ABVector = transform.position - CharacterCamera.transform.position;
         ABVector.y = 0;
-        float angle = Vector3.Angle(ABVector, GlassRotate*Vector3.forward);
+        float angle = Vector3.Angle(ABVector, (GlassRotate*Vector3.right));
         Debug.Log(angle);
         if (FixAxis == "X")
         {
@@ -54,7 +59,7 @@ public class ScanCamera : MonoBehaviour
             {
                 Cm.fieldOfView = Field_Of_View + (Mathf.Abs(Distance - 8) * 8f);
                 float TargetP = (CharacterCamera.transform.position.x - transform.position.x) * (1 / Distance);
-                transform.position = Vector3.Lerp(transform.position, new Vector3(Mathf.Clamp(CharacterCamera.transform.position.x, OriginAxisPosition.x + MinP * (Mathf.Clamp((1 / Distance),0,1)), OriginAxisPosition.x + MaxP * (Mathf.Clamp((1 / Distance), 0, 1))), transform.position.y, transform.position.z), 1f);
+                transform.position = Vector3.Lerp(transform.position, new Vector3(Mathf.Clamp(CharacterCamera.transform.position.x, OriginAxisPosition.x - MinP * (Mathf.Clamp((1 / Distance),0,4)), OriginAxisPosition.x - MaxP * (Mathf.Clamp((1 / Distance), 0, 1))), transform.position.y, transform.position.z), 1f);
             }
             else
             {
@@ -80,5 +85,12 @@ public class ScanCamera : MonoBehaviour
             }
         }
         transform.position = new Vector3(transform.position.x, CharacterCamera.transform.position.y + CameraY_Offset, transform.position.z);
+    }
+
+    private Vector3 OriginTargetRotation;
+    private Vector3 OriginRotation;
+    private void CameraFix2()
+    {
+        transform.eulerAngles = OriginRotation + (TargetGlass.eulerAngles - OriginTargetRotation);
     }
 }
