556f9250ead2becc34b6a059381fd1eba5867afc
kennuki <henry0002277@gmail.com>
2023-11-28 02:49:38 +0800

    11//28

--- Assets/Scenes/level2/Script/2-5/Friend_Move.cs
+++ Assets/Scenes/level2/Script/2-5/Friend_Move.cs
@@ -8,13 +8,36 @@ public class Friend_Move : MonoBehaviour
     public FacePoint_Player facePoint;
     private Transform nowPoint,nextPoint;
     private float player_move_dis;
+    private float counter = 0;
+    private bool move_to_center = false;
+    private Vector3 posValue;
+    public Vector3 PosValue
+    {
+        get { return posValue; }
+        set
+        {
+            if (posValue != value)
+            {
+                posValue = value;
+                move_to_center = true;
+            }
+        }
+    }
     private void Update()
     {
         nowPoint = AllArea.Instance.friend_point;
         if (nextTarget.target != null)
             nextPoint = nextTarget.target;
         player_move_dis = facePoint.dis_has_moved;
-        move();
+        if (!move_to_center)
+        {
+            move();
+        }
+        else
+        {
+            MoveToCenter();
+        }
+        PosValue = nowPoint.position;
     }
     private void move()
     {
@@ -22,8 +45,22 @@ public class Friend_Move : MonoBehaviour
         if (nextTarget.target != null&&nowPoint!=null)
         {
             Vector3 dir = (nextPoint.position - nowPoint.position).normalized;
-            Vector3 target_pos = nowPoint.position + dir * player_move_dis * 1.1f;
-            transform.position = Vector3.Lerp(transform.position, target_pos, 0.2f);
+            Vector3 target_pos = nowPoint.position + dir * player_move_dis * 1f;
+            transform.position = Vector3.Lerp(transform.position, target_pos, 0.05f);
         }
     }
+    private void MoveToCenter()
+    {
+        counter += Time.deltaTime;
+        if (nextTarget.target != null && nowPoint != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, nowPoint.position, 0.02f);
+        }
+        if (counter > 0.3f)
+        {
+            move_to_center = false;
+            counter = 0;
+        }
+    }
+    
 }
